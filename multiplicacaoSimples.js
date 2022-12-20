99dd58683a1908f160d3dbb380b83a03354ebfbf
Glenilson Santil <49415313+GlenilsonSantil@users.noreply.github.com>
2022-12-20 17:08:15 -0300

    Adicionado arquivo mulriplicaçãoSimples,js

---
+++ multiplicacaoSimples.js
@@ -0,0 +1,14 @@
+// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente e têm as seguintes funcionalidades: 
+// - "gets()" : lê UMA linha com dados de entrada (inputs) do usuário;
+// - "print()": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente;
+// - "split()": divide uma String em um Array de Strings;
+
+// Abaixo segue um exemplo de código que você pode ou não utilizar:
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+
+//TODO: Complete os espaços em branco utilizando uma possível solução para o desafio:
+
+let total = valor1 * valor2;
+print("PROD = " + total);
